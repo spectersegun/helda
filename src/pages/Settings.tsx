7323c1bdc3044d585104c9e2235c1cf49b0d7fe0
specter segun <segunomojolowo@gmail.com>
2025-09-27 17:10:23 +0100

    latest

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -8,12 +8,17 @@ export default function Settings() {
       <div className="!p-6 !h-full  bg-[#f3f3ee] flex gap-5 font-outfit">
         <HeldaSidebar2 activeKey="settings" />
 
-        <div className="flex-1">
+        <div className="flex-1 flex flex-col gap-5 ">
           <WelcomeCard
             name="Settings"
             avatarSrc="/images/dp.png"
             className="!shadow-[0_1px_2px_rgba(0,0,0,0.06),0_2px_6px_rgba(0,0,0,0.05)]"
           />
+
+          <div className="grid grid-cols-2 gap-5 items-stretch flex-1 !relative !pt-10 justify-center ">
+            <div className="col-span-1"></div>
+            <div className="col-span-1"></div>
+          </div>
         </div>
       </div>
     </GreenWrapper>
