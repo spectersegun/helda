9f704eb689f89fe33f44528323802809c9047143
specter segun <segunomojolowo@gmail.com>
2025-10-01 13:25:15 +0100

    complete build

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import animationWhiteBgUpd1 from "./animationWhiteBgUpd1.gif";
-import { Frame } from "./Frame";
+// import { Frame } from "./Frame";
 import "./Image.css";
 
 interface ImageProps {
