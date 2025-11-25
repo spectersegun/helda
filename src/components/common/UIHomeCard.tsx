00926e360e31b942e0e680bec0c565020ee18bde
specter segun <segunomojolowo@gmail.com>
2025-11-25 17:07:22 +0100

    UIHomeCard update

--- src/components/common/UIHomeCard.tsx
+++ src/components/common/UIHomeCard.tsx
@@ -14,9 +14,9 @@ export default function UIHomeCard({
   negativeArrow?: boolean;
 }) {
   return (
-    <div className="flex flex-col gap-[0.56vh] items-center !py-[0.6vw] !px-[0.6vw] h-[13.9vh] bg-white rounded-[1vw] text-center ">
-      <div className="flex items-center !mb-[1.11vh] fall-in">
-        <h3 className="!mb-0 !text-[1.2vw] !font-semibold max-w-[16vw] ">
+    <div className="flex flex-col gap-[0.56vh] items-center !py-[0.6vw] !px-[0.6vw] h-[14.352vh] bg-white rounded-[1vw] text-center">
+      <div className="flex items-center fall-in">
+        <h3 className="!mb-0 !text-[1.2vw] !font-semibold max-w-[16vw] min-h-[5.370vh] ">
           <span className="inline-block align-middle !mr-[0.4vw] ">
             <StarIcon width={18} height={18} className="!h-[1.6vh] !w-auto" />
           </span>
@@ -24,15 +24,17 @@ export default function UIHomeCard({
         </h3>
       </div>
 
-      <p className="text-[#1F664B] text-[0.909vw] font-medium !mb-0 leading-[1vw] fall-in">
-        {positiveArrow && <span> ▲</span>}
-        {positive}
-      </p>
+      <div>
+        <p className="text-[#1F664B] text-[0.909vw] font-medium !mb-0 leading-[1vw] fall-in">
+          {positiveArrow && <span> ▲</span>}
+          {positive}
+        </p>
 
-      <p className="text-[#FD0303] text-[0.909vw] font-medium !mb-0 fall-in">
-        {negativeArrow && <span>▼ </span>}
-        {nagative}
-      </p>
+        <p className="text-[#FD0303] text-[0.909vw] font-medium !mb-0 fall-in">
+          {negativeArrow && <span>▼ </span>}
+          {nagative}
+        </p>
+      </div>
     </div>
   );
 }
